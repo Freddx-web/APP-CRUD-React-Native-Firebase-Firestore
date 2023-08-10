1d5faf81214da0741847fe23572d5b652fe5745a
Fredd <66976840+Freddx-web@users.noreply.github.com>
2023-08-09 21:35:56 -0600

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+
 import Home from './screens/Home';
 import Add from './screens/Add';
 
